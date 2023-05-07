f8a5004810b14231d62b36ebff83cdd9bcafc0e7
Muhammad Usama Arshad <131895089+MuhammadUsama101@users.noreply.github.com>
2023-05-07 12:00:05 +0500

    My code

---
+++ Programs/044ReplaceSpaces.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string given="My name is usama";
+    string temp="";
+    for(int i=0;i<given.length();i++){
+        if(given[i]==' '){
+            temp.push_back('@');
+            temp.push_back('4');
+            temp.push_back('0');
+        }
+        else {
+            temp.push_back(given[i]);
+        }
+    }
+    cout<<temp;
+}
+// --------------------------------------------------
+//            using vector technique 
+// --------------------------------------------------
+    // string given="My name is usama";
+    // vector<string> v;
+    // string temp="";
+    // for( auto i:given){
+    //     if(i==' '){
+    //     temp+="@40";
+    //     }
+    //     else 
+    //      temp+=i;
+    // }
+    // v.push_back(temp);
+    // for(auto i:temp){
+    //     cout<<i;
+    // }
